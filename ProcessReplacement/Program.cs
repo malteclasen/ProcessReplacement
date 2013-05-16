d05345f57cfe3ddf1d2384fc4c74905cf5062b56
malte.clasen <info@malteclasen.de>
2013-05-17 00:49:32 +0200

    initial commit

---
+++ ProcessReplacement/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessReplacement
+{
+	class Program
+	{
+		private const string EventSource = "Process Replacement";
+		private const string EventLog = "Application";
+
+		static Process GetParentProcess(int childProcessId)
+		{
+			var query = string.Format("SELECT ParentProcessId FROM Win32_Process WHERE ProcessId = {0}", childProcessId);
+			var search = new ManagementObjectSearcher("root\\CIMV2", query);
+			var results = search.Get().GetEnumerator();
+			if (!results.MoveNext()) return null;
+			var queryObj = results.Current;
+			uint parentId = (uint)queryObj["ParentProcessId"];
+			try
+			{
+				return Process.GetProcessById((int) parentId);
+			}
+			catch (ArgumentException)
+			{
+				return null;
+			}			
+		}
+
+		static void RecordCall(string[] args)
+		{
+			var message = new StringBuilder();
+			message.AppendLine("Process Replacement started");
+			message.AppendLine("Arguments: " + string.Join(" ", args));
+
+			AppendParentProcessInfo(message, Process.GetCurrentProcess().Id);
+
+			System.Diagnostics.EventLog.WriteEntry(EventSource, message.ToString());			
+		}
+
+		private static void AppendParentProcessInfo(StringBuilder message, int childProcessId)
+		{
+			var parent = GetParentProcess(childProcessId);
+			if (parent != null)
+			{
+				try
+				{
+					message.AppendLine(
+						string.Format("Parent:\n- Name={0}\n- FileName={1}\n- FileDescription={2}\n- CompanyName={3}\n",
+									  parent.ProcessName,
+									  parent.MainModule.FileName,
+									  parent.MainModule.FileVersionInfo.FileDescription,
+									  parent.MainModule.FileVersionInfo.CompanyName
+							));
+				}
+				catch (Win32Exception)
+				{
+					message.AppendLine(
+						string.Format("Parent:\n- Name={0}\n",
+									  parent.ProcessName
+							));
+				}
+
+				AppendParentProcessInfo(message, parent.Id);
+			}
+			else
+			{
+				message.AppendLine("Could not determine parent process.");
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			try
+			{
+				if (!System.Diagnostics.EventLog.SourceExists(EventSource))
+					System.Diagnostics.EventLog.CreateEventSource(EventSource, EventLog);
+			}
+			catch (SecurityException)
+			{
+				Console.WriteLine("Please run this program once with administrator rights to enable event logging.");
+			}
+			RecordCall(args);
+			Console.WriteLine(@"This process has been redirected, the call has been recorded in the windows event log {0} as {1}. Please check the registry keys in HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\ to run whatever you intended to run.", EventLog, EventSource);
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 ProcessReplacement
 ==================
+
+I observed strange behavior on my notebook: At apparently random times, bcdedit.exe was started and used one CPU core at 100%. It did not stop on its own, and it refused to be terminated by force. ProcessExplorer reported only "access denied", Xperf showed no stack trace, only a full reboot could stop it. Anti-virus tools could not find anything, no trojan, no rootkit, no virus, no ad-ware. At this point, I decided to debug this in a somewhat unconventional way. I wrote this Process Replacement tool, which logs details about parent processes to the Windows event log when started. Then I registered it as a debugging replacement for bcdedit in the Windows registry:
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\bcdedit.exe]
+"Debugger"="C:\\Users\\malte.clasen\\Documents\\Projects\\ProcessReplacement\\ProcessReplacement\\bin\\Release\\ProcessReplacement.exe"
+
+Now everytime someone tried to run bcdedit.exe, ProcessReplacement is started instead. I quickly found out that the SoftThinks Agent Service was responsible, which is apparently a part of the Dell Backup and Recovery tool. Now I still have to work out how to deal with this, but at least I know where to start :)
